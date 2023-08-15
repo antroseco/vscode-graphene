2093d101f9ba4bccb3824491fe7738b5bf09070a
Antros Economides <antroseconomides@hotmail.co.uk>
2023-08-15 22:51:02 +0300

    completions: add snippets for `for` loops

--- src/completions.js
+++ src/completions.js
@@ -36,7 +36,27 @@ function activate(context) {
         }
     });
 
-    context.subscriptions.push(kw_provider);
+    const sn_provider = vscode.languages.registerCompletionItemProvider("graphene", {
+        provideCompletionItems(document, position, token, context) {
+            const for_sn = new vscode.CompletionItem(
+                { label: "for", description: "Code snippet for for loop" }
+            );
+            for_sn.insertText = new vscode.SnippetString(
+                "for ${1:i} in ${2:iterator} {\n\t${0:$BLOCK_COMMENT_START code $BLOCK_COMMENT_END}\n}"
+            );
+
+            const for_range_sn = new vscode.CompletionItem(
+                { label: "forrange", description: "Code snippet for range-based for loop" }
+            );
+            for_range_sn.insertText = new vscode.SnippetString(
+                "for ${1:i} in range(${2:upper}) {\n\t${0:$BLOCK_COMMENT_START code $BLOCK_COMMENT_END}\n}"
+            );
+
+            return [for_sn, for_range_sn]
+        }
+    });
+
+    context.subscriptions.push(kw_provider, sn_provider);
 }
 
 
