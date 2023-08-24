d77261de2d38a14fb16812c4cca64c6420d6b23f
Antros Economides <antroseconomides@hotmail.co.uk>
2023-08-24 13:07:53 +0300

    completions: automatically require `std/iterators.c3` on `forrange`

--- src/completions.js
+++ src/completions.js
@@ -22,6 +22,35 @@ const GRAPHENE_KEYWORDS = [
 ];
 
 /**
+ * @param {vscode.TextDocument} document
+ * @param {string} path
+ */
+function document_requires_path(document, path) {
+    // XXX this doesn't accept all valid `@require_once` statements; e.g.
+    // `@require_once /* comment */ "path"`, but there's not much we can do
+    // until we get our own language server to parse the document.
+    const regexp = new RegExp(`^\\s*@require_once\\s*"${path}"\\s*$`, "gm");
+
+    return regexp.test(document.getText());
+}
+
+/**
+ * @param {vscode.TextDocument} document
+ */
+function position_after_last_requires(document) {
+    // Use a negative look-ahead to find the last occurence.
+    const regexp = /^(?=\s*)@require_once\s*"[^"]*"(?!.*^\s*@require_once)/sm
+    const match = document.getText().match(regexp);
+
+    // No match found, so return the start of the document.
+    if (match === null)
+        return new vscode.Position(0, 0);
+
+    // Return the line after the last match.
+    return document.positionAt(match.index).translate(1);
+}
+
+/**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
@@ -51,6 +80,16 @@ function activate(context) {
             for_range_sn.insertText = new vscode.SnippetString(
                 "for ${1:i} in range(${2:upper}) {\n\t${0:$BLOCK_COMMENT_START code $BLOCK_COMMENT_END}\n}"
             );
+
+            // Automatically include `std/iterators.c3` if it's not already
+            // included.
+            if (!document_requires_path(document, "std/iterators.c3")) {
+                const for_range_te = vscode.TextEdit.insert(
+                    position_after_last_requires(document),
+                    '@require_once "std/iterators.c3"\n'
+                );
+                for_range_sn.additionalTextEdits = [for_range_te];
+            }
 
             const req_sn = new vscode.CompletionItem(
                 { label: "require", description: "Code snippet for @require_once" }
